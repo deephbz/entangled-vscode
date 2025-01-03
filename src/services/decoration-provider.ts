9fa0f22509a184e88d1a14e10c6153f4f8580ff4
TechnoHouse (deephbz) <13776377+deephbz@users.noreply.github.com>
2025-01-03 18:44:11 +0800

    definition hightlight

--- src/services/decoration-provider.ts
+++ src/services/decoration-provider.ts
@@ -1,18 +1,28 @@
 import * as vscode from 'vscode';
-import { DecorationConfig, defaultDecorationConfig } from '../config/decoration';
+import { defaultDecorationConfig } from '../config/decoration';
 
 export class DecorationProvider {
     private static instance: DecorationProvider;
-    private config: DecorationConfig;
-
-    private definitionDecoration!: vscode.TextEditorDecorationType;
-    private continuationDecoration!: vscode.TextEditorDecorationType;
-    private inBlockRefDecoration!: vscode.TextEditorDecorationType;
-    private outBlockRefDecoration!: vscode.TextEditorDecorationType;
+    private definitionDecorationType: vscode.TextEditorDecorationType;
+    private continuationDecorationType: vscode.TextEditorDecorationType;
+    private inBlockRefDecorationType: vscode.TextEditorDecorationType;
+    private outBlockRefDecorationType: vscode.TextEditorDecorationType;
+    private referenceDecorationType: vscode.TextEditorDecorationType;
 
     private constructor() {
-        this.config = defaultDecorationConfig;
-        this.createDecorationTypes();
+        this.definitionDecorationType = vscode.window.createTextEditorDecorationType({
+            backgroundColor: new vscode.ThemeColor('entangled.definition.background'),
+            color: new vscode.ThemeColor('entangled.definition.foreground'),
+            rangeBehavior: vscode.DecorationRangeBehavior.ClosedClosed
+        });
+        this.continuationDecorationType = vscode.window.createTextEditorDecorationType(defaultDecorationConfig.continuationStyle);
+        this.inBlockRefDecorationType = vscode.window.createTextEditorDecorationType(defaultDecorationConfig.inBlockReferenceStyle);
+        this.outBlockRefDecorationType = vscode.window.createTextEditorDecorationType(defaultDecorationConfig.outBlockReferenceStyle);
+        this.referenceDecorationType = vscode.window.createTextEditorDecorationType({
+            backgroundColor: new vscode.ThemeColor('entangled.reference.background'),
+            color: new vscode.ThemeColor('entangled.reference.foreground'),
+            rangeBehavior: vscode.DecorationRangeBehavior.ClosedClosed
+        });
     }
 
     public static getInstance(): DecorationProvider {
@@ -22,33 +32,34 @@ export class DecorationProvider {
         return DecorationProvider.instance;
     }
 
-    private createDecorationTypes(): void {
-        this.definitionDecoration = vscode.window.createTextEditorDecorationType(this.config.definitionStyle);
-        this.continuationDecoration = vscode.window.createTextEditorDecorationType(this.config.continuationStyle);
-        this.inBlockRefDecoration = vscode.window.createTextEditorDecorationType(this.config.inBlockReferenceStyle);
-        this.outBlockRefDecoration = vscode.window.createTextEditorDecorationType(this.config.outBlockReferenceStyle);
-    }
-
     public updateDecorations(editor: vscode.TextEditor): void {
-        const document = editor.document;
-        if (document.languageId !== 'entangled-markdown') {
+        if (editor.document.languageId !== 'entangled-markdown') {
             return;
         }
 
-        const text = document.getText();
+        const text = editor.document.getText();
         const definitionRanges: vscode.Range[] = [];
         const continuationRanges: vscode.Range[] = [];
         const inBlockRefRanges: vscode.Range[] = [];
         const outBlockRefRanges: vscode.Range[] = [];
+        const referenceRanges: vscode.Range[] = [];
+
+        // Find all definitions (#name)
+        const defRegex = /(?<=^```\s*\{[^}]*#)([^\s}]+)(?=[^}]*\})/gm;
+        let match;
+        while ((match = defRegex.exec(text)) !== null) {
+            const startPos = editor.document.positionAt(match.index);
+            const endPos = editor.document.positionAt(match.index + match[1].length);
+            definitionRanges.push(new vscode.Range(startPos, endPos));
+        }
 
         // Match code block headers with identifiers
         const codeBlockRegex = /```\{[^}]*?#([^\s}]+)[^}]*?\}/g;
-        let match;
         let lastBlockEnd = 0;
         
         while ((match = codeBlockRegex.exec(text))) {
-            const startPos = document.positionAt(match.index);
-            const endPos = document.positionAt(match.index + match[0].length);
+            const startPos = editor.document.positionAt(match.index);
+            const endPos = editor.document.positionAt(match.index + match[0].length);
             const range = new vscode.Range(startPos, endPos);
             
             // Check if there's an unclosed code block before this one
@@ -57,7 +68,7 @@ export class DecorationProvider {
             
             if (blockCount % 2 === 0) {
                 // This is a new block definition
-                definitionRanges.push(range);
+                // definitionRanges.push(range);
             } else {
                 // This is a continuation of a previous block
                 continuationRanges.push(range);
@@ -66,12 +77,12 @@ export class DecorationProvider {
             lastBlockEnd = match.index + match[0].length;
         }
 
-        // Match references
-        const referenceRegex = /<<([^>]+)>>/g;
-        while ((match = referenceRegex.exec(text))) {
-            const startPos = document.positionAt(match.index);
-            const endPos = document.positionAt(match.index + match[0].length);
-            const range = new vscode.Range(startPos, endPos);
+        // Find all references (<<name>>)
+        const refRegex = /<<([^>]+)>>/g;
+        while ((match = refRegex.exec(text)) !== null) {
+            const startPos = editor.document.positionAt(match.index);
+            const endPos = editor.document.positionAt(match.index + match[0].length);
+            referenceRanges.push(new vscode.Range(startPos, endPos));
 
             // Find the nearest code block markers before this reference
             const textBefore = text.substring(0, match.index);
@@ -79,23 +90,24 @@ export class DecorationProvider {
             const isInCodeBlock = blockMarkers.length % 2 === 1;
 
             if (isInCodeBlock) {
-                inBlockRefRanges.push(range);
+                inBlockRefRanges.push(new vscode.Range(startPos, endPos));
             } else {
-                outBlockRefRanges.push(range);
+                outBlockRefRanges.push(new vscode.Range(startPos, endPos));
             }
         }
 
-        // Apply decorations
-        editor.setDecorations(this.definitionDecoration, definitionRanges);
-        editor.setDecorations(this.continuationDecoration, continuationRanges);
-        editor.setDecorations(this.inBlockRefDecoration, inBlockRefRanges);
-        editor.setDecorations(this.outBlockRefDecoration, outBlockRefRanges);
+        editor.setDecorations(this.definitionDecorationType, definitionRanges);
+        editor.setDecorations(this.continuationDecorationType, continuationRanges);
+        editor.setDecorations(this.inBlockRefDecorationType, inBlockRefRanges);
+        editor.setDecorations(this.outBlockRefDecorationType, outBlockRefRanges);
+        editor.setDecorations(this.referenceDecorationType, referenceRanges);
     }
 
     public dispose(): void {
-        this.definitionDecoration.dispose();
-        this.continuationDecoration.dispose();
-        this.inBlockRefDecoration.dispose();
-        this.outBlockRefDecoration.dispose();
+        this.definitionDecorationType.dispose();
+        this.continuationDecorationType.dispose();
+        this.inBlockRefDecorationType.dispose();
+        this.outBlockRefDecorationType.dispose();
+        this.referenceDecorationType.dispose();
     }
 }
